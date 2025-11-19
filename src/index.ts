995776ee78f4dbff2de53ed595da10ed04a10035
Mervin González <gonzalez.mervin@gmail.com>
2025-11-18 22:13:25 -0300

    commit 1

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import products from "./products.json";
+
 class ListaDeCosas {
   name: string;
-  cosas: any[] = [];
+  cosas: Product[] = [];
   constructor(name: string) {
     // nombre de esta lista
     this.name = name;
@@ -24,6 +26,57 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name: string) {
+    super(name);
+
+    const productosIniciales = products as Product[];
+
+    for (const producto of productosIniciales) {
+      this.addProduct(producto);
+    }
+
+    this.addProduct(new Product("Producto Nuevo", 999, 4));
+  }
+
+  addProduct(product: Product): void {
+    let existe = false;
+
+    for (const element of this.cosas) {
+      if (product.id === element.id) {
+        existe = true;
+        break;
+      }
+    }
+    if (!existe) {
+      this.cosas.push(product);
+    }
+  }
+
+  getProduct(id: number): Product | undefined {
+    for (const product of this.cosas) {
+      if (product.id === id) {
+        return product;
+      }
+    }
+    return undefined;
+  }
+
+  removeProduct(id: number): void {
+    this.cosas = this.cosas.filter((product) => product.id !== id);
+  }
+
+  getSortedByPrice(order: string): Product[] {
+    const sortedProducts = [...this.cosas];
+    if (order === "asc") {
+      return sortedProducts.sort((a, b) => a.price - b.price);
+    } else if (order === "desc") {
+      return sortedProducts.sort((a, b) => b.price - a.price);
+    } else {
+      console.warn("El orden especificado no es válido. Usa 'asc' o 'desc'.");
+      return sortedProducts;
+    }
+  }
+}
 
 export { ListaDeProductos, Product };
